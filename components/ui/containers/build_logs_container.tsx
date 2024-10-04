58083b80368f762006ceebfedbfd0e8a0efeb58b
Vikas Patil <vikaspatil0021o+github@gmail.com>
2024-10-04 20:14:24 +0530

    if deploying_status is false then reftech one last time

--- components/ui/containers/build_logs_container.tsx
+++ components/ui/containers/build_logs_container.tsx
@@ -18,7 +18,7 @@ type Log = {
 
 export default function BuildLogsContainer({ deployment_id, deploying_status }: { deployment_id: string, deploying_status: boolean }) {
 
-    const { data: logs_data, isError, isFetching, isRefetching, error } = trpc?.build_logs?.get_logs.useQuery({ deployment_id }, { refetchOnWindowFocus: false, refetchInterval: (deploying_status ? 6000 : false) })
+    const { data: logs_data, isError, isFetching, isRefetching, error, refetch } = trpc?.build_logs?.get_logs.useQuery({ deployment_id }, { refetchOnWindowFocus: false, refetchInterval: (deploying_status ? 6000 : false) })
 
     useEffect(() => {
 
@@ -31,15 +31,22 @@ export default function BuildLogsContainer({ deployment_id, deploying_status }: 
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [isError])
 
+    useEffect(() => {
+        if (!deploying_status) {
+            refetch()
+        }
+    }, [deploying_status])
+
     return (
         <>
             <div className="text-muted-foreground text-xs px-3 mt-2 md:p-0">
                 Build logs
             </div>
+
             <ScrollArea className="relative flex-1 mt-1 bg-[#333] rounded-sm mx-3 md:mx-0 px-3">
                 {isError && <div className="text-center mt-5 text-sm text-muted-foreground">Something went wrong</div>}
-                {!isError && !isRefetching && isFetching && <div className="flex mt-5 justify-center"><LoadingIcon /></div>}
-                {!isError && !isFetching && logs_data?.length === 0 && <div className="text-center mt-5 text-sm text-muted-foreground">No Logs Found</div>}
+                {!isError && (deploying_status && logs_data?.length === 0 || (!isRefetching && isFetching)) && <div className="flex mt-5 justify-center"><LoadingIcon /></div>}
+                {!isError && !isFetching && !isRefetching && !deploying_status && logs_data?.length === 0 && <div className="text-center mt-5 text-sm text-muted-foreground">No Logs Found</div>}
                 {!isError && (!isFetching || isRefetching) && logs_data &&
                     logs_data?.map((log_event: Log, index: number) => {
                         return (
